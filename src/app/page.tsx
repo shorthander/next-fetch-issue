73945de8cdd753c8f017c13a5e9f5b5e62033132
Adrian Rinnus <adrian.rinnus@gmail.com>
2024-03-19 08:36:28 +0100

    Add display for workaround and buggy version.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,18 @@ export default async function Home() {
     const {data: workaroundData, error: workaroundError} = await getDataWORKAROUND()
 
     return (
-        <div className={'flex flex-col gap-6'}>
-            <div>
-                <pre><code>Bug - fetch(finalUrl, {headers: headers()});</code></pre>
-                <div className={'border border-white rounded'}>data: {data.text}</div>
-                <div className={'border border-white rounded'}>error: {error}</div>
+        <div className={'space-y-6 divide-y-2 '}>
+            <div className={'flex flex-col gap-2'}>
+                <pre><code>{`Bug - fetch(finalUrl, {headers: headers()});`}</code></pre>
+                <div className={'p-4 border border-white rounded'}>data: {data.text}</div>
+                <div className={'p-4 border border-red-500 rounded'}>error: {error}</div>
             </div>
-            <div>
-                <pre><code>Bug - fetch(finalUrl, {headers: new Headers(headers())});</code></pre>
-                <div className={'border border-white rounded'}>data: {workaroundData.text}</div>
-                <div className={'border border-white rounded'}>error: {workaroundError}</div>
+            <div className={'space-y-2 pt-6'}>
+                <pre><code>{`Bug - fetch(finalUrl, {headers: new Headers(headers())});`}</code></pre>
+                <div className={'p-4 border border-white rounded'}>data: {workaroundData.text}</div>
+                <div className={'p-4 border border-red-500 rounded'}>error: {workaroundError}</div>
             </div>
-
         </div>
+
     );
 }
